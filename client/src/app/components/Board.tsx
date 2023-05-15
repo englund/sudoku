d976be0c92b63313315d60822a941db9845817e9
Victor Englund <victor.englund@gmail.com>
2023-05-15 21:33:11 +0200

    make board a bit more beautiful

--- client/src/app/components/Board.tsx
+++ client/src/app/components/Board.tsx
@@ -5,39 +5,55 @@ import { FC, useState } from "react";
 const sizeOfBoard = 9;
 
 const getNewBoard = (size: number) =>
-  Array.from(new Array<number>(size), () =>
-    Array.from(new Array<number>(size), () => 0)
+  Array.from(new Array<string>(size), () =>
+    Array.from(new Array<string>(size), () => "")
   );
 
-const Board: FC = () => {
-  const [board, setBoard] = useState(getNewBoard(sizeOfBoard));
+const getNewBoard2 = (size: number) => {
+  let board = getNewBoard(size);
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      board[x][y] = `${x}${y}`;
+    }
+  }
+  return board;
+};
 
-  const updateValue = (x: number, y: number, value: number) => {
+const Board: FC = () => {
+  const [board, setBoard] = useState(getNewBoard2(sizeOfBoard));
+
+  const updateValue = (x: number, y: number, value: string) => {
     const newBoard = [...board];
     newBoard[x][y] = value;
     setBoard(newBoard);
   };
 
   return (
-    <>
+    <div className="[&>*:nth-child(3n+4)]:border-t-4">
       {board.map((row, x) => {
         return (
-          <div key={x}>
-            {row.map((col, y) => (
-              <input // TODO: make input type text and validate by hand to make it possible for empty values
-                key={`${x}-${y}`}
-                type=""
-                value={col}
-                onChange={(e) => updateValue(x, y, +e.target.value)}
-                min={0}
-                max={9}
-                className="w-6 bg-black outline-0"
-              />
-            ))}
+          <div
+            key={x}
+            className="grid grid-cols-9 [&>*:nth-child(3n+4)]:border-l-4"
+          >
+            {row.map((value, y) => {
+              return (
+                <div key={`${x}-${y}`} className="w-12 h-12">
+                  <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => updateValue(x, y, e.target.value)} // TODO: validate input
+                    min={0}
+                    max={9}
+                    className="w-6 bg-black outline-0"
+                  />
+                </div>
+              );
+            })}
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 

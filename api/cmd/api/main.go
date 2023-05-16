10d3ad2ac853bb742c067b017d040181b8f84211
Victor Englund <victor.englund@gmail.com>
2023-05-16 16:10:47 +0200

    add status endpoint

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"net/http"
+	"sudoku/api/pkg/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	routes.Status(r.Group("/status"))
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
